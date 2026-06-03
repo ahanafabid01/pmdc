810d4ea516808b276a59d4c46df810a1a660098c
Ahanaf Abid Sazid <155557684+ahanafabid01@users.noreply.github.com>
2026-06-03 21:33:40 +0600

    refactor: remove legacy gallery.php redirect file

--- gallery.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* gallery.php was moved to pages/gallery.php.
-   This file redirects any bookmarks or old links automatically. */
-header('HTTP/1.1 301 Moved Permanently');
-header('Location: pages/gallery.php');
-exit;
